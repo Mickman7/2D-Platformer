1f48e9fa0126725aaa0d1191369cf87ba769937e
Mickman7 <101274574+Mickman7@users.noreply.github.com>
2022-11-30 22:02:04 +0000

    Add files via upload

---
+++ ItemCollector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int apples = 0;
+
+    [SerializeField] private Text ApplesText;
+
+    [SerializeField] private AudioSource collectionSoundEffect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Apple"))
+        {
+            collectionSoundEffect.Play();
+            Destroy(collision.gameObject);
+            apples++;
+            ApplesText.text = "Apples: " + apples;
+        }
+    }
+}
---
+++ PlayerLife.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        anim.SetTrigger("Death");
+    }
+}
